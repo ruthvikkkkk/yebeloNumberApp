ed10eaab79af8d24ae311b86f03d981ab6d8ea52
jakkaruthvikkumar <jakkaruthvik@gmail.com>
2023-06-20 22:38:07 +0530

    project init

---
+++ src/main/java/com/example/yebeloNumberSum/YebeloNumberSumApplication.java
@@ -0,0 +1,13 @@
+package com.example.yebeloNumberSum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebeloNumberSumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebeloNumberSumApplication.class, args);
+	}
+
+}
